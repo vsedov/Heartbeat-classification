6313b5f27a4cb6b0d383ee51ca6e44230b48c5d0
vsedov <viv.sedov@hotmail.com>
2022-08-30 16:40:00 +0100

    feat(Constants): Better constants

--- heart/constants.py
+++ heart/constants.py
@@ -1,3 +1,9 @@
+import os
+from typing import Optional
+
 import torch
 
-DEFAULT_DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+TRACE_LOGGERS: list[str] = [f"!{__name__}"]
+DEFAULT_DEVICE: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+DIR: Optional[str] = os.path.dirname(__file__)
+DATASET_DIR = os.path.join(DIR, "data/mitbih_ds/")
--- heart/utils/helper.py
+++ heart/utils/helper.py
@@ -1,5 +1,3 @@
-import os
-
 import matplotlib.pyplot as plt
 import torch
 from torchinfo import summary
@@ -8,16 +6,38 @@ from heart.constants import DEFAULT_DEVICE
 
 
 def to_default_device(*pointer):
+    """Quick way to force default device to cuda"""
     for output in pointer:
         yield output.to(DEFAULT_DEVICE)
 
 
 def show_summary(network):
+    """Show summary of a network"""
     summary(network)
 
 
 def train_epoch(net, data_loader, loss_fn, optimizer=None, lr=0.01):
+    """
+    Train epoch
+    Parameters
+    ----------
+    net : Neural network layer
+        This can be a class or something that you have defined, that will contain the basis of all the network layer and
+        structure
+    dataloader : DataLoader
+        Which defines the data to trian on, this can be the test_data or training_data
+    lr : Learning_Rate
+        Learning rate, defines the speed at which the network would learn from, this is done through gradient descent,
+        How large of a jump do you want the network to take ?
+    optimizer : Optimizer
+        Default optimizer is the Adam optimizer, but this is the algorithm that you will be using :Adam
+        so far
+    loss_fn : loss_function
+        default Negative log likehood function
 
+    Returns
+    -------
+    """
     optimizer = optimizer or torch.optim.Adam(net.parameters(), lr=lr)
     net.train()
     total_loss, acc, count = 0, 0, 0
@@ -36,6 +56,14 @@ def train_epoch(net, data_loader, loss_fn, optimizer=None, lr=0.01):
 
 
 def plot_results(hist):
+    """
+    Plot results : based on training acc and validation acc
+
+    Parameters
+    ----------
+    hist : Data from the labels
+        shows a plot
+    """
     plt.figure(figsize=(15, 5))
     plt.subplot(121)
     plt.plot(hist['train_acc'], label='Training acc')
