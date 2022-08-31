6dd6475c4e70bab4b9f8b389723225ca8c94ee99
vsedov <viv.sedov@hotmail.com>
2022-08-31 11:45:00 +0100

    refactor(constants): better documentation for constants. and update gitignore , use script to download the datasets

---
+++ heart/__init__.py
@@ -0,0 +1,3 @@
+from heart.log import setup
+
+setup()
--- heart/constants.py
+++ heart/constants.py
@@ -2,8 +2,31 @@ import os
 from typing import Optional
 
 import torch
+from dotenv import load_dotenv
 
+load_dotenv()
+#  ╭────────────────────────────────────────────────────────────────────╮
+#  │                                                                    │
+#  │ log                                                                │
+#  │                                                                    │
+#  ╰────────────────────────────────────────────────────────────────────╯
 TRACE_LOGGERS: list[str] = [f"!{__name__}"]
+
+#  ╭────────────────────────────────────────────────────────────────────╮
+#  │                                                                    │
+#  │ torch                                                              │
+#  │                                                                    │
+#  ╰────────────────────────────────────────────────────────────────────╯
 DEFAULT_DEVICE: str = 'cuda' if torch.cuda.is_available() else 'cpu'
-DIR: Optional[str] = os.path.dirname(__file__)
-DATASET_DIR = os.path.join(DIR, "data/mitbih_ds/")
+BATCH_SIZE = 64
+DEFAULT_LOSS = torch.nn.LogSoftmax
+DEFAULT_OPTIM = torch.optim.Adam
+#  ╭────────────────────────────────────────────────────────────────────╮
+#  │                                                                    │
+#  │ Directory information                                              │
+#  │                                                                    │
+#  ╰────────────────────────────────────────────────────────────────────╯
+DIR = os.path.dirname(os.path.abspath(__file__))
+DATASET_DIR = os.path.join(DIR, "data/heartbeat/")
+KAGGLE_USERNAME = os.getenv("KAGGLE_USERNAME")
+KAGGLE_KEY = os.getenv("KAGGLE_KEY")
