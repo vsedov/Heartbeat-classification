2fccb160c46a06cd980bdbab4731fc87b82405b8
vsedov <viv.sedov@hotmail.com>
2022-09-02 13:35:38 +0100

    feat(vis): Class to support viewing dataset and nn

---
+++ heart/visualization/visualize.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import torch.nn as nn
+
+from heart.log import get_logger
+
+log = get_logger(__name__)
+
+
+def visualise_network(net: nn.Sequential):
+    weight_input = next(net.parameters())
+    fig, ax = plt.subplot(1, 10, figsize=(15, 4))
+    for i, x in enumerate(weight_input):
+        #  TODO: (vsedov) (13:25:07 - 02/09/22): Finish this function
+        ax[i].imshow(x.view(28, 28).detach())
+    ax.show()
+
+
+def show_data(hist):
+    plt.figure(figsize=(15, 5))
+    plt.subplot(121)
+    plt.plot(hist['train_acc'], label='Training acc')
+    plt.plot(hist['val_acc'], label='Validation acc')
+    plt.legend()
+    plt.subplot(122)
+    plt.plot(hist['train_loss'], label='Training loss')
+    plt.plot(hist['val_loss'], label='Validation loss')
+    plt.legend()
+    plt.show()
+
+
+def visualize_data_loader(data_loader):
+    for i, (x, y) in enumerate(data_loader):
+        log.info(x.shape, y.shape)
+        plt.imshow(x[0].view(28, 28).detech())
+        plt.show()
+        if i > 10:
+            break
