0345c2714e29975612caba022e902b3d3f810903
vsedov <viv.sedov@hotmail.com>
2022-09-12 01:15:26 +0100

    feat(autoencoder): due to how auto encoders work . We have to have a unique process of how to load data , main thing to note is teh loss data.

--- heart/models/validation.py
+++ heart/models/validation.py
@@ -20,10 +20,27 @@ def validate(model, loader, loss_fn):
             logits = model(x)
             current_loss = loss_fn(logits, y)
             predicted = hp.filtered_acc(logits, loss_fn)
+
+            predictred = logits.argmax()
             loss += current_loss.item() * feat.size(0)
-            if loss_fn.__class__.__name__ != "MSELoss":
-                correct += torch.eq(predicted, y).sum().float().item()
-            else:
-                correct = (logits - y).abs().sum().item()
+            correct += torch.eq(predicted, y).sum().float().item()
 
     return loss / total, correct / total
+
+
+def validate_ae(model, loader, loss_fn):
+    model.eval()
+    loss = 0, 0
+    val_bar = tqdm(loader, file=sys.stdout)
+    loss = []
+    for i, (feat, lbl) in enumerate(val_bar):
+        x, y = hp.to_default_device(feat, lbl)
+        with torch.no_grad():
+            logits = model(x)
+            current_loss = loss_fn(logits, y)
+            predicted = hp.filtered_acc(logits, loss_fn)
+            val_bar.desc = f"val loss:{current_loss:.3f}"
+
+            if i % 10:
+                loss.append(float(current_loss))
+    return loss
