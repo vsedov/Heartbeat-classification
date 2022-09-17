40558f6abaea97358641d90de4ac81ef21fbdcfc
vsedov <viv.sedov@hotmail.com>
2022-09-17 03:02:19 +0100

    refactor(model config): refactor __main__ due to changes in model_run

--- heart/__main__.py
+++ heart/__main__.py
@@ -1,5 +1,4 @@
-from heart.model_run.all import setup
+from heart.model_run.all import setup_all
 
 #  TODO: (vsedov) (19:45:40 - 08/09/22): Do something with this data
-data = setup()
-
+data = setup_all()
